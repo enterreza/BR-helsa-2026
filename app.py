dd27ff0f9682b98f96b5dce0207e79bd7f3391e4
enterreza <101956463+enterreza@users.noreply.github.com>
2026-04-11 11:54:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 import os
 
 # --- KONFIGURASI HALAMAN ---
-st.set_page_config(page_title="Dashboard Keuangan RS", layout="wide", page_icon="📈")
+st.set_page_config(page_title="Helsa Rumah Sakit", layout="wide", page_icon="📈")
 
 # --- TAMBAHAN LOGO DI POJOK KIRI ATAS ---
 LOGO_FILE = "HELSA Rumah sakit.png"
